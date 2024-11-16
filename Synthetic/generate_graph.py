74ce8ec406b306e4d8a5aa8acaf912c84d33ca6f
cc7738@kit.edu <cc7738@uc2n996.localdomain>
2024-11-16 16:21:20 +0100

    Final version

--- Synthetic/generate_graph.py
+++ Synthetic/generate_graph.py
@@ -5,28 +5,40 @@ from typing import *
 import random
 import numpy as np
 import networkx as nx
+import argparse
+import seaborn as sns
+
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-import torch_geometric.transforms as T
 from matplotlib.pyplot import cm
+from matplotlib.figure import Figure
+import scipy.sparse as sp
+
+from torch_sparse import SparseTensor
+import torch_geometric.transforms as T
 from torch_geometric.data import Data
-from torch_geometric.utils import from_networkx
+from torch_geometric.transforms import RandomLinkSplit
+from torch_geometric.utils import from_networkx, to_undirected
 
 class SyntheticGraphGeneration:
     SQUARE = 0
     TRIANGLE = 1
     HEXAGONAL = 2
+    FILE_PATH = '/home/kit/aifb/cc7738/scratch/Universal-MP/Synthetic/pictures'
     
-    def __init__(self, m: int, n: int, emb_dim: int, graph_type: int, heterophily: bool=False, homophily: bool=False, feature_type: str='random'):
-        self.m = m
-        self.n = n
-        self.emb_dim = emb_dim
-        self.graph_type = graph_type
-        self.heterophily = heterophily
-        self.homophily = homophily
+    def __init__(self, m: int, n: int, emb_dim: int, graph_type: int, heterophily: bool=False, 
+                 homophily: bool=False, feature_type: str='random', cfg: argparse.Namespace=None):
+        self.m            = m
+        self.n            = n
+        self.emb_dim      = emb_dim
+        self.graph_type   = graph_type
+        self.heterophily  = heterophily
+        self.homophily    = homophily
         self.feature_type = feature_type
+        self.cfg          = cfg
 
-    def plot_graph(self, G: nx.Graph, pos: Optional[Dict[int, Tuple[float, float]]] = None, title: str = "Graph", node_size: int = 300, node_color: str = 'skyblue', with_labels: bool = True) -> Figure:
+    def plot_graph(self, G: nx.Graph, pos: Optional[Dict[int, Tuple[float, float]]] = None,
+                   title: str = "Graph", node_size: int = 300, node_color: str = 'skyblue', 
+                   with_labels: bool = True) -> Figure:
         """
         Input:
         ----------
@@ -59,7 +71,8 @@ class SyntheticGraphGeneration:
         plt.title(title)
         return plt.gcf()
 
-    def plot_color_graph(self, G: nx.Graph, pos: Optional[Dict[int, Tuple[float, float]]] = None, title: str = "Graph", node_size: int = 300, with_labels: bool = True) -> Figure:
+    def plot_color_graph(self, G: nx.Graph, pos: Optional[Dict[int, Tuple[float, float]]] = None, 
+                         title: str = "Graph", node_size: int = 300, with_labels: bool = True) -> Figure:
         """
         Input:
         ----------
@@ -86,13 +99,13 @@ class SyntheticGraphGeneration:
         node_labels: Dict[int, Any] = nx.get_node_attributes(G, 'label')
         
         unique_labels: Set[Any] = set(node_labels.values())
-        print(node_labels)
+        
         colors: List[str] = ['red', 'black']  # Add more colors if you have more labels
         
         color_map: Dict[Any, str] = {label: colors[i % len(colors)] for i, label in enumerate(unique_labels)}
         
         node_color: List[str] = [color_map[node_labels[node]] for node in G.nodes]
-        print(node_color)
+        
         plt.figure(figsize=(10, 8))
         nx.draw(G, pos, with_labels=with_labels, node_size=node_size, node_color=node_color, 
                 edge_color='gray', font_size=10)
@@ -113,7 +126,7 @@ class SyntheticGraphGeneration:
 
         Description:
         -----------
-        This function plots a histogram of the node degree frequency distribution for a graph.
+            This function plots a histogram of the node degree frequency distribution for a graph.
 
         """
         
@@ -137,6 +150,58 @@ class SyntheticGraphGeneration:
         ax.set_xlim(0, 10)
         
         return fig
+
+    def calculate_common_neighbors(self, edge_index: torch.Tensor, A: 'scipy.sparse.csr_matrix', batch_size: int = 10000) -> torch.FloatTensor:
+        """
+        Input:
+        - edge_index (torch.Tensor): A tensor of shape (2, E), where E is the number of edges. 
+        - A ('scipy.sparse.csr_matrix'): The adjacency matrix of the graph in CSR (Compressed Sparse Row) format.
+        - batch_size (int): The batch size for processing pairs of nodes. Default is 10000.
+
+        Output:
+        - torch.FloatTensor: A tensor containing the number of common neighbors for each pair of nodes in `edge_index`.
+
+        Description:
+            This function computes the number of common neighbors for each pair of nodes in `edge_index` using the adjacency matrix `A`.
+        """
+        link_loader = torch.utils.data.DataLoader(range(edge_index.size(1)), batch_size)
+        scores = []
+
+        for ind in link_loader:
+            src, dst = edge_index[0, ind].to('cpu'), edge_index[1, ind].to('cpu')
+            cur_scores = np.array(A[src].multiply(A[dst]).sum(axis=1)).flatten()
+            scores.append(cur_scores)
+
+        scores = np.concatenate(scores, axis=0)
+        return torch.FloatTensor(scores)
+
+    def plot_common_neighbors_distribution(self, train_cn: torch.FloatTensor, test_cn: torch.FloatTensor) -> None:
+        """
+        Input:
+        - train_cn (torch.FloatTensor): A tensor containing the number of common neighbors for the training dataset.
+        - test_cn (torch.FloatTensor): A tensor containing the number of common neighbors for the testing dataset.
+
+        Output:
+        - None: The function saves the plot of the distribution as a PNG file.
+
+        Description:
+        This function generates a density plot of the number of common neighbors for both the training and testing datasets
+        using Kernel Density Estimation (KDE) with the Seaborn library. It visualizes the distribution and saves the plot
+        to a specified file path.
+        """
+
+        # Convert tensors to NumPy arrays for plotting
+        train_cn = train_cn.to('cpu').numpy()
+        test_cn = test_cn.to('cpu').numpy()
+        print(train_cn)
+        plt.figure(figsize=(10, 6))
+        sns.kdeplot(train_cn, label='Training', color='red', linewidth=2)
+        sns.kdeplot(test_cn, label='Testing', color='blue', linewidth=2)
+        plt.xlabel('Number of Common Neighbors')
+        plt.ylabel('Density')
+        plt.legend()
+        plt.title('Distribution of Common Neighbors')
+        plt.savefig(f'{SyntheticGraphGeneration.FILE_PATH}/common_neighbours_distribution.png')
 
     def generate_node_features(self, G: nx.Graph) -> torch.Tensor:
         """
@@ -280,15 +345,15 @@ class SyntheticGraphGeneration:
         nodes: List[Tuple[int, int]] = list(G.nodes())
         num_nodes: int = len(nodes)
 
-        # Define the number of nodes for each label (half nodes get label 0, half get label 1)
-        half_nodes: int = num_nodes // 2
+        if self.homophily:
+            half_nodes: int = num_nodes // 2
 
-        # Assign labels to nodes
-        for i, node in enumerate(nodes):
-            if i < half_nodes:
-                G.nodes[node]['label'] = 0
-            else:
-                G.nodes[node]['label'] = 1
+            # Assign labels to nodes
+            for i, node in enumerate(nodes):
+                if i < half_nodes:
+                    G.nodes[node]['label'] = 0
+                else:
+                    G.nodes[node]['label'] = 1
                 
         return G, pos
 
@@ -338,6 +403,59 @@ class SyntheticGraphGeneration:
 
         return G, pos
 
+    def get_edge_split(self, data: Data,
+                   undirected: bool,
+                   device: Union[str, int],
+                   val_pct: float,
+                   test_pct: float,
+                   split_labels: bool,
+                   include_negatives: bool = False) -> Dict[str, Data]:
+        """
+        Input:
+        -----------
+        data : Data
+            The input graph data in the PyTorch Geometric format.
+        undirected : bool
+            Specifies whether the graph is undirected. If True, edges will be treated as bidirectional.
+        device : Union[str, int]
+            The device to which the data will be moved (e.g., 'cpu', 'cuda', or a specific GPU ID).
+        val_pct : float
+            The percentage of edges to be used for validation.
+        test_pct : float
+            The percentage of edges to be used for testing.
+        split_labels : bool
+            If True, includes the labels for split edges (positive and negative) in the returned datasets.
+        include_negatives : bool, default=False
+            If True, includes negative samples (non-existent edges) in the training data for better evaluation of link prediction.
+
+        Returns:
+        --------
+        Dict[str, Data]
+            A dictionary containing the split data:
+            - 'train': The training dataset.
+            - 'valid': The validation dataset.
+            - 'test': The test dataset.
+
+        Description:
+        ------------
+            This function splits the dataset edges into training, validation, and test sets.
+        """
+        transform = T.Compose([
+            T.NormalizeFeatures(),
+            T.ToDevice(device),
+            RandomLinkSplit(is_undirected=undirected,
+                            num_val=val_pct,
+                            num_test=test_pct,
+                            add_negative_train_samples=include_negatives,
+                            split_labels=split_labels),
+
+        ])
+        # del data.adj_t, data.e_id, data.batch_size, data.n_asin, data.n_id
+        train_data, val_data, test_data = transform(data)
+        # Delete negative samples from train dataset
+        del train_data.neg_edge_label, train_data.neg_edge_label_index
+        return {'train': train_data, 'valid': val_data, 'test': test_data}
+
     def generate_graph(self) -> Tuple[Data, torch.Tensor, torch.Tensor, Dict[int, Tuple[float, float]]]:
         """
         Output:
@@ -361,17 +479,18 @@ class SyntheticGraphGeneration:
             raise ValueError(f"Invalid graph type: {self.graph_type}")
         
         plt = self.plot_graph(G, pos, title=f"{self.graph_type} Graph")
-        plt.savefig(f'/home/kit/aifb/cc7738/scratch/Universal-MP/Synthetic/pictures/{self.graph_type}_grid_graph.png')
+        plt.savefig(f'{SyntheticGraphGeneration.FILE_PATH}/{self.graph_type}_grid_graph.png')
         
         if self.homophily or self.heterophily:
             plt = self.plot_color_graph(G, pos, title=f"{self.graph_type} Graph")
-            plt.savefig(f'/home/kit/aifb/cc7738/scratch/Universal-MP/Synthetic/pictures/{self.graph_type}_grid_graph_color.png')
+            plt.savefig(f'{SyntheticGraphGeneration.FILE_PATH}/{self.graph_type}_grid_graph_color.png')
         
         plt = self.plot_degree_histogram(G)
-        plt.savefig(f'/home/kit/aifb/cc7738/scratch/Universal-MP/Synthetic/pictures/{self.graph_type}_grid_graph_degree_hist.png')
+        plt.savefig(f'{SyntheticGraphGeneration.FILE_PATH}/{self.graph_type}_grid_graph_degree_hist.png')
         
         # Convert G: networkx -> Data
         data: Data = from_networkx(G)
+        data.weight = torch.ones(data.edge_index.size(1), dtype=float)
         data = self.rename_fields(data)
         weights: torch.Tensor = data.edge_weight.clone()
         
@@ -384,6 +503,38 @@ class SyntheticGraphGeneration:
         # Creating edge_index and return back edge_weight
         row, col, _ = data.adj_t.coo()
         data.edge_index = torch.stack([col, row], dim=0)
-        data.edge_weight = weights
         
-        return data, data.adj_t, data.x, pos
+        if self.cfg.undirected:
+            edge_index = to_undirected(data.edge_index, weights, reduce='add')
+            data.edge_index = edge_index[0]
+            data.edge_weight = weights #edge_index[1]
+            data.adj_t = sp.csr_matrix((data.edge_weight.cpu(), (data.edge_index[0].cpu(), data.edge_index[1].cpu())),
+                            shape=(data.num_nodes, data.num_nodes))
+            # data.adj_t = SparseTensor(row=data.edge_index[0], 
+            #                           col=data.edge_index[1], 
+            #                           value=data.edge_weight.to(torch.float32))
+        else:
+            data.adj_t = sp.csr_matrix((weights.cpu(), (data.edge_index[0].cpu(), data.edge_index[1].cpu())), 
+                     shape=(data.num_nodes, data.num_nodes))
+            data.edge_weight = weights
+        
+        # Splitting edges
+        splits = self.get_edge_split(data,
+                   self.cfg.undirected,
+                   self.cfg.device,
+                   self.cfg.val_pct,
+                   self.cfg.test_pct,
+                   self.cfg.split_labels,
+                   self.cfg.include_negatives)
+        
+        train_edge_index = splits['train']['pos_edge_label_index']
+        test_edge_index = splits['test']['pos_edge_label_index']
+
+        # Calculate common neighbors
+        train_cn = self.calculate_common_neighbors(train_edge_index, data.adj_t)
+        test_cn = self.calculate_common_neighbors(test_edge_index, data.adj_t)
+
+        # Plot common neighbors distribution
+        self.plot_common_neighbors_distribution(train_cn, test_cn)
+        print(data.num_nodes)
+        return data, data.adj_t, data.x, splits
